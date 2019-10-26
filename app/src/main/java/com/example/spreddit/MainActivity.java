0c22ff759c38286271200973af939116ced9aa86
NikhilMJagtap <44296369+NikhilMJagtap@users.noreply.github.com>
2019-10-26 16:59:16 +0530

    Added tabs and fragments

--- app/src/main/java/com/example/spreddit/MainActivity.java
+++ app/src/main/java/com/example/spreddit/MainActivity.java
@@ -2,38 +2,82 @@ package com.example.spreddit;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+//import com.example.spreddit.ui.main.SectionsPagerAdapter;
 
-import com.example.spreddit.ui.main.SectionsPagerAdapter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int[] tabIcons = {R.drawable.ic_home_black_24dp, R.drawable.ic_apps_black_24dp, R.drawable.ic_mode_edit_black_24dp,
+            R.drawable.ic_chat_black_24dp, R.drawable.ic_account_circle_black_24dp};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+//        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
+//        setupViewPager(viewPager);
+//        viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
+//        tabs.setupWithViewPager(viewPager);
+        ViewPageAdapter adapter = new ViewPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new homeFragment(), null);
+        adapter.addFragment(new subFragment(), null);
+        adapter.addFragment(new postFragment(), null);
+        adapter.addFragment(new messageFragment(), null);
+        adapter.addFragment(new profileFragment(), null);
+        viewPager.setAdapter(adapter);
         tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        for(int i=0; i<adapter.getCount(); ++i)
+            tabs.getTabAt(i).setIcon(tabIcons[i]);
+        System.out.println("Here");
 
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+//    private void setupViewPager(ViewPager viewPager){
+//        SectionsPagerAdapter adapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+////        adapter.
+//        viewPager.setAdapter(adapter);
+//    }
+
+}
+
+class ViewPageAdapter extends FragmentPagerAdapter{
+    private final List<Fragment> mfragmentList = new ArrayList<>();
+    private final List<String> mfragmentTitleList = new ArrayList<>();
+
+    public ViewPageAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mfragmentList.get(position);
+    }
+
+    @Override
+    public int getCount(){
+        return mfragmentList.size();
+    }
+
+    public void addFragment(Fragment f, String s){
+        mfragmentList.add(f);
+        mfragmentTitleList.add(s);
+    }
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position){
+        return mfragmentTitleList.get(position);
     }
 }
---
+++ app/src/main/java/com/example/spreddit/homeFragment.java
@@ -0,0 +1,23 @@
+package com.example.spreddit;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class homeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
